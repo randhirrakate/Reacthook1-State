1fe8d8b7d548f3652a068f12d79a924c0472fb53
randhirrakate <randhirrakate2610@gmail.com>
2021-02-12 15:47:29 +0530

    reacthook1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    my_name :"Randhir"
 }
+
+  changeMyName = () => {
+    this.setState({
+      my_name :"Wisdom"
+    })
+  }
+
+  render()
+  {
+    return(
+      <div>
+        <h1>Hello, {this.state.my_name}</h1>
+
+        <button onClick={this.changeMyName}>Click Here</button>
+      </div>
+    );
+  };
+  
+};
 
 export default App;
